dba537223cd723b7ad70c0f4fca3141b78cd1d22
Corwin <korwindrus@gmail.com>
2017-08-08 01:43:29 +0300

    Fix links.

--- Wiki_prototype/Parser.cpp
+++ Wiki_prototype/Parser.cpp
@@ -115,7 +115,7 @@ void link_identification(vector <wchar_t> &word, vector <wchar_t> &str, map <str
 				insert(str, 0, L"<a href=", 8);
 				str.insert(str.end(), wchar_t('"'));
 				if (!dict["mlink"]) dict["flink"] = 1;
-				return;
+				break;
 			}
 		}
 
